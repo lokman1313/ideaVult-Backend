886a64524f5c1478aa208f39024c63388a3b824e
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-21 01:00:26 +0600

    verificetion added on many page

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const verifiToken =async (req,res,next)=>{
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("ideaVult")
     const ideaCollection = db.collection("ideas")
     const commentCollection =db.collection("comment")
@@ -170,8 +170,8 @@ async function run() {
     })
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
    
     // await client.close();
